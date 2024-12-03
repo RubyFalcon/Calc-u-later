6351b53bcf427225c311ebebef09357bc9f84022
Tupay Felkin <61619620+RubyFalcon@users.noreply.github.com>
2024-12-03 15:19:44 +0000

    refactored to accept muliple operators

--- script.js
+++ script.js
@@ -1,109 +1,172 @@
-const add = (...args) => args.reduce((accum, val) => accum + val, 0);
-  
-  const subtract = (...args) => args.reduce((accum, val) => accum - val, 0);
-  
-  const sum = function(arr) {
-    if(arr.length == 0 ){
-      return 0;
-    }
-    else if (arr.length <2){
-  
-      return arr[0];
-    }
-      else{
-      return arr.reduce((start, next)=> start+next, 0) 
-    }
-  };
 
-  const multiply = (...args) => args.reduce((accum, val) => args.length === 0 ? 0:  accum * val, 1);
+let displayValue = 0;
+let firstOperator = null;
+let firstNum = null;
+let secondOperator = null;
+let secondNum =null;
+let result;
+const buttons = document.querySelectorAll('button');
 
-  const divide = (...args) => args.reduce((accum, val) => args.length === 0? 0: val / accum, 1);
-  
+function displayNum() {
+  const display = document.getElementById('display');
+  display.innerText = displayValue;
+  if(displayValue.length > 9) {
+      display.innerText = displayValue.substring(0, 9);
+  }
+}
 
-
- 
-
-
-  const displayedNum =  document.querySelector("#display");
-
-
-// todo-- handle if 0 then h1 = 0 else if !0
-const numbers = document.querySelectorAll(".number");
-numbers.forEach(number => {
-  number.addEventListener("click", ()=>  {
-    let newNum = number.value;
-
-    if (displayedNum.textContent == '0' ){
-      displayedNum.textContent = newNum;
-    } else {
-      displayedNum.textContent += newNum;
-    }
-    
+displayNum();
+buttons.forEach(function (button) {
+  button.addEventListener("click", () => {
+    if(button.classList.contains('number')) {
+      inputOperand(button.value);
+      displayNum();
+  } else if(button.classList.contains('operator')) {
+      inputOperator(button.value);
+  } else if(button.classList.contains('equals')) {
+      inputEquals();
+      displayNum();
+  } else if(button.classList.contains('decimal')) {
+      inputDecimal(button.value);
+      displayNum();
+  } else if(button.classList.contains('percent')) {
+      inputPercent(displayValue);
+      displayNum();
+  } else if(button.classList.contains('clear'))
+      clearDisplay();
+      displayNum();
   })
 })
-// console.table(numbers)
 
-
-function operate(operator,aString , bString) {
-    if(bString == ""){
-      return aString;
-    }
-    let operatedNum;
-    a = parseInt(aString);
-    b = parseInt(bString);
-    switch(operator){
-      case "/":
-        operatedNum = divide(a,b);
-        break;
-      case "*":
-        operatedNum = multiply(a,b);
-        break;
-      case "+":
-        operatedNum = add(a,b);
-        break;
-      case "-":
-        operatedNum = subtract(a,b);
-        break;
-    }
-    return operatedNum;
-}
-
-function clearContent(){
-  displayedNum.textContent = "0";
-}
-
-const clear= document.querySelector(".clear")
-clear.addEventListener("click",()=> clearContent());
-
-
-const  operators = document.querySelectorAll('.operator');
-let prevOperator = "";
-let prevValue = "";
-let isNewValue = false;
-let nextValue= "";
-operators.forEach((operator)=> {
-  operator.addEventListener("click", ()=> {
-    let prevValue = displayedNum.textContent;
-
-
-    if(operator.value != "=") {
-      prevOperator = operator.value;
-      nextValue = prevValue;
-      displayedNum.textContent = "";
-      
-      
-    }
-    else if (operator.value == "=" ) {
-          
-          
-          let operatedValue = operate(prevOperator,prevValue, nextValue);
-          displayedNum.textContent = operatedValue
-          prevValue = operatedValue;
-
+function inputOperand(operand) {
+    if(firstOperator === null) {
+        if(displayValue === '0' || displayValue === 0) {
+            //1st click - handles first operand input
+            displayValue = operand;
+        } else if(displayValue === firstNum) {
+            //starts new operation after inputEquals()
+            displayValue = operand;
+        } else {
+            displayValue += operand;
         }
-      
-    
+    } else {
+        //3rd/5th click - inputs to secondNum
+        if(displayValue === firstNum) {
+            displayValue = operand;
+        } else {
+            displayValue += operand;
+        }
+    }
+}
+
+function inputOperator(operator) {
+    if(firstOperator != null && secondOperator === null) {
+        //4th click - handles input of second operator
+        secondOperator = operator;
+        secondNum = displayValue;
+        result = operate(Number(firstNum), Number(secondNum), firstOperator);
+        displayValue = roundAccurately(result, 15).toString();
+        firstNum = displayValue;
+        result = null;
+    } else if(firstOperator != null && secondOperator != null) {
+        //6th click - new secondOperator
+        secondNum = displayValue;
+        result = operate(Number(firstNum), Number(secondNum), secondOperator);
+        secondOperator = operator;
+        displayValue = roundAccurately(result, 15).toString();
+        firstNum = displayValue;
+        result = null;
+    } else { 
+        //2nd click - handles first operator input
+        firstOperator = operator;
+        firstNum = displayValue;
+    }
+}
+
+function inputEquals() {
+  //hitting equals doesn't display undefined before operate()
+  if(firstOperator === null) {
+      displayValue = displayValue;
+  } else if(secondOperator != null) {
+      //handles final result
+      secondNum = displayValue;
+      result = operate(Number(firstNum), Number(secondNum), secondOperator);
+      if(result === 'lmao') {
+          displayValue = 'lmao';
+      } else {
+          displayValue = roundAccurately(result, 15).toString();
+          firstNum = displayValue;
+          secondNum = null;
+          firstOperator = null;
+          secondOperator = null;
+          result = null;
+      }
+  } else {
+      //handles first operation
+      secondNum = displayValue;
+      result = operate(Number(firstNum), Number(secondNum), firstOperator);
+      if(result === 'lmao') {
+          displayValue = 'lmao';
+      } else {
+          displayValue = roundAccurately(result, 15).toString();
+          firstNum = displayValue;
+          secondNum = null;
+          firstOperator = null;
+          secondOperator = null;
+          result = null;
+      }
   }
-    
-  )
-})
+}
+
+function inputDecimal(dot) {
+  if(displayValue === firstNum || displayValue === secondNum) {
+      displayValue = '0';
+      displayValue += dot;
+  } else if(!displayValue.includes(dot)) {
+      displayValue += dot;
+  } 
+}
+
+function inputPercent(num) {
+  displayValue = (num/100).toString();
+}
+
+function inputSign(num) {
+  displayValue = (num * -1).toString();
+}
+
+function clearDisplay() {
+  displayValue = '0';
+  firstNum = null;
+  secondNum = null;
+  firstOperator = null;
+  secondOperator = null;
+  result = null;
+}
+
+function inputBackspace() {
+  if(firstNum != null) {
+      firstNum = null;
+      updateDisplay();
+  }
+}
+
+function operate(x, y, op) {
+  if(op === '+') {
+      return x + y;
+  } else if(op === '-') {
+      return x - y;
+  } else if(op === '*') {
+      return x * y;
+  } else if(op === '/') {
+      if(y === 0) {
+          return 'lmao';
+      } else {
+      return x / y;
+      }
+  }
+}
+
+function roundAccurately(num, places) {
+  return parseFloat(Math.round(num + 'e' + places) + 'e-' + places);
+}
